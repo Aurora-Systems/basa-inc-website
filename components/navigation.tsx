009aa2d260e923d079c39183109f517a242908ad
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2025-11-29 18:33:41 +0200

    Update logo image source in navigation component

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
         <div className="flex justify-center align-center align-middle text-center font-bold">
           <div>
-            <img src="https://ngratesc.sirv.com/basainc/logo.png" alt="logo" width={"50"}/>
+            <img src="https://cdn.clipond.com/basainc/logo.png" alt="logo" width={"50"}/>
           </div>
           <div className="pt-2">
             <small >The World Is Yours</small>
